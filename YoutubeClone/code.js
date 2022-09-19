7e4f4dc058350fd5da81f48385b13adf3bae1d69
Cobra <cjbrjk@gmail.com>
2022-09-19 18:07:48 -0500

    Fifth version of the application (just visual, non functional)

---
+++ YoutubeClone/code.js
@@ -0,0 +1,8 @@
+// const picture = document.getElementsByClassName("video-thumbnail-container");
+// console.log(2);
+// console.log(picture);
+
+// picture.addEventListener("mouseover", appear, false);
+// picture.addEventListener("mouseout", disppear, false);
+
+// appear
